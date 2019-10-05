0d8ed00d4a1e3bf7c08b18f8f7e4b827d009129a
willianpassarelli <devpassarelli@gmail.com>
2019-10-04 22:43:57 -0300

    layout signup and signin

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="Meetup" />
+
+      <Form>
+        <Input name="name" type="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Digite seu email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/_layouts/auth/styles.js
+++ src/pages/_layouts/auth/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
   height: 100%;
@@ -12,4 +13,49 @@ export const Content = styled.div`
   width: 100%;
   max-width: 315px;
   text-align: center;
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 50px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+      font-weight: 700;
+    }
+
+    button {
+      margin: 5px 0 0;
+      height: 50px;
+      background: #f94d6a;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+
+      &:hover {
+        background: ${darken(0.03, '#f94d6a')};
+      }
+    }
+
+    a {
+      color: #fff;
+      margin-top: 20px;
+      font-size: 16px;
+      opacity: 0.6;
+      transition: opacity 0.2s;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
 `;
