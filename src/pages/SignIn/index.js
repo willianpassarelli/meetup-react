88839a5a4df6ce409db84161b284fcfb348498ee
willianpassarelli <devpassarelli@gmail.com>
2019-10-04 22:15:53 -0300

    layout auth

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import logo from '~/assets/logo.svg';
+
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="Meetup" />
+
+      <Form>
+        <Input name="email" type="email" placeholder="Digite seu email" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Entrar</button>
+        <Link to="/register">Criar conta grátis</Link>
+      </Form>
+    </>
+  );
+}
