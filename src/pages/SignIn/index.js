934db6d9446ebab4fa4518a91f5080f635439317
willianpassarelli <devpassarelli@gmail.com>
2019-10-06 15:42:44 -0300

    add redux in signin and signup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 
+import * as Yup from 'yup';
+
 import logo from '~/assets/logo.svg';
 
+import { signInRequest } from '~/store/modules/auth/actions';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email válido')
+    .required('O email é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
   return (
     <>
       <img src={logo} alt="Meetup" />
 
-      <Form>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Digite seu email" />
         <Input
           name="password"
